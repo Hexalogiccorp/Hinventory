0dacfb3b6b4b5307fb6cc75d6662c8a7af42b735
QuiqueCode <dylanbusisness@gmail.com>
2025-11-12 14:26:44 -0600

    update: updating sidbar to minibar

--- src/app/shared/components/sidebar-component/sidebar-component.ts
+++ src/app/shared/components/sidebar-component/sidebar-component.ts
@@ -1,11 +1,13 @@
+import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar-component',
-  imports: [MatSidenavModule, MatButtonModule, RouterModule],
+  imports: [MatSidenavModule, MatButtonModule, RouterModule,MatTooltipModule,NgClass],
   templateUrl: './sidebar-component.html',
   styleUrl: './sidebar-component.css'
 })
