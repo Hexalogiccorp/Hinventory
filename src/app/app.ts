e4d02a6149ac561402a62a3397b0f0f178f0c277
diegoTech14 <diegotech1499@gmail.com>
2025-11-13 07:04:46 -0600

    Deleted code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CardComponent } from '@shared/components/card-component/card-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
