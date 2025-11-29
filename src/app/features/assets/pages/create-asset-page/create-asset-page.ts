56cdc45d87fd95e386c8ed5248872c9686440b2a
QuiqueCode <dylanbusisness@gmail.com>
2025-11-29 11:17:14 -0600

    feat/create asset page

---
+++ src/app/features/assets/pages/create-asset-page/create-asset-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-asset-page',
+  imports: [],
+  templateUrl: './create-asset-page.html',
+  styleUrl: './create-asset-page.css'
+})
+export class CreateAssetPage {
+
+}
