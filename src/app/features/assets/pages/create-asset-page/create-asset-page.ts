075d156ae76a381d989b97ef0711507353de74c7
QuiqueCode <dylanbusisness@gmail.com>
2025-11-29 15:11:43 -0600

    feat/crate asset page

--- src/app/features/assets/pages/create-asset-page/create-asset-page.ts
+++ src/app/features/assets/pages/create-asset-page/create-asset-page.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { TabsComponent } from '@shared/components/tabs-component/tabs-component';
+import { AddAssetFormComponent } from '../../components/add-asset-form-component/add-asset-form-component';
+import { AddCategoryFormComponent } from '../../components/add-category-form-component/add-category-form-component';
 
 @Component({
   selector: 'app-create-asset-page',
-  imports: [],
+  imports: [TabsComponent,AddAssetFormComponent,AddCategoryFormComponent],
   templateUrl: './create-asset-page.html',
   styleUrl: './create-asset-page.css'
 })
 export class CreateAssetPage {
+
+  arrayComponents=[
+    {icon:"arrow_circle_up",name:"Activos",component:AddAssetFormComponent},
+    {icon:"category",name:"Categorías",component:AddCategoryFormComponent}
+  ]
 
 }
