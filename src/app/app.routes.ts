04df262051af75dcb6d2c68b8c249be01a06b7e9
QuiqueCode <dylanbusisness@gmail.com>
2025-12-09 14:04:45 -0600

    feat/routing and navigation project done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AssetPage } from './features/assets/pages/asset-page/asset-page';
+import { DashboardPage } from './features/dashboard/pages/dashboard-page/dashboard-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'asset' , component:AssetPage},
+    {path:'dashboard', component:DashboardPage},
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+];
